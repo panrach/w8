cc5428d5fce0994d7d538a22f9c9da309d85795c
panrach <126734747+panrach@users.noreply.github.com>
2024-07-02 13:13:13 -0400

    Create list.c

---
+++ list.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// Please use the man pages of opendir etc to find out more #include's you need.
+
+int main(int argc, char *argv[])
+{
+  if (argc < 2) {
+    fprintf(stderr, "not enough arguments\n");
+    return 1;
+  }
+
+
+  return 0;
+}
